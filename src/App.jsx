6d986149bc63b43a7fbe7b12ee6947edb052d75f
mhuss03 <134786667+mhuss03@users.noreply.github.com>
2024-10-10 12:44:35 +0100

    toggle switch added

--- src/App.jsx
+++ src/App.jsx
@@ -128,8 +128,8 @@ function App() {
   const [data, setData] = useState([]);
   const [currentSurah, setCurrentSurah] = useState([]);
   const [surahNumber, setSurahNumber] = useState(1);
-  const [showTranslation, setShowTranslation] = useState(true);
-  const [showNote, setShowNote] = useState(true);
+  const [showTranslation, setShowTranslation] = useState({});
+  const [showNote, setShowNote] = useState({});
 
   // Fetch Data From JSON
   async function fetchData() {
@@ -150,6 +150,20 @@ function App() {
         (x) => x["Surah No"] === `${surahNumber}`,
       );
       setCurrentSurah(filteredData);
+
+      //Setting All Translation -> True
+      const initialShowTranslation = filteredData.reduce((acc, ayah) => {
+        acc[ayah.id] = true;
+        return acc;
+      }, {});
+      setShowTranslation(initialShowTranslation);
+
+      //Setting All Showing Note  -> True
+      const initialShowNote = filteredData.reduce((acc, ayah) => {
+        acc[ayah.id] = true;
+        return acc;
+      }, {});
+      setShowNote(initialShowNote);
     }
   }, [surahNumber, data]);
 
@@ -159,18 +173,41 @@ function App() {
   };
 
   // Toggle Note/Translation Visibilty
-  const toggleVisibility = (ayahId, hideOption) => {
-    if (hideOption === "translation") {
+  const toggleVisibility = (ayahId, option) => {
+    // hide/show translation
+    if (option === "translation") {
       setShowTranslation((prevVisible) => ({
         ...prevVisible,
         [ayahId]: !prevVisible[ayahId],
       }));
-    } else {
+    }
+    // hide/show translation
+    else if (option === "note") {
       setShowNote((prevVisible) => ({
         ...prevVisible,
         [ayahId]: !prevVisible[ayahId],
       }));
     }
+  };
+
+  // Translation Toggle (ALL)
+  const onTranslationChange = (e) => {
+    const isChecked = e.target.checked;
+    const updatedShowTranslation = currentSurah.reduce((acc, ayah) => {
+      acc[ayah.id] = isChecked;
+      return acc;
+    }, {});
+    setShowTranslation(updatedShowTranslation);
+  };
+
+  // Note Toggle (ALL)
+  const onNoteChange = (e) => {
+    const isChecked = e.target.checked;
+    const updatedShowNote = currentSurah.reduce((acc, ayah) => {
+      acc[ayah.id] = isChecked;
+      return acc;
+    }, {});
+    setShowNote(updatedShowNote);
   };
 
   return (
@@ -229,7 +266,7 @@ function App() {
             >
               <path d="M 10.490234 2 C 10.011234 2 9.6017656 2.3385938 9.5097656 2.8085938 L 9.1757812 4.5234375 C 8.3550224 4.8338012 7.5961042 5.2674041 6.9296875 5.8144531 L 5.2851562 5.2480469 C 4.8321563 5.0920469 4.33375 5.2793594 4.09375 5.6933594 L 2.5859375 8.3066406 C 2.3469375 8.7216406 2.4339219 9.2485 2.7949219 9.5625 L 4.1132812 10.708984 C 4.0447181 11.130337 4 11.559284 4 12 C 4 12.440716 4.0447181 12.869663 4.1132812 13.291016 L 2.7949219 14.4375 C 2.4339219 14.7515 2.3469375 15.278359 2.5859375 15.693359 L 4.09375 18.306641 C 4.33275 18.721641 4.8321562 18.908906 5.2851562 18.753906 L 6.9296875 18.1875 C 7.5958842 18.734206 8.3553934 19.166339 9.1757812 19.476562 L 9.5097656 21.191406 C 9.6017656 21.661406 10.011234 22 10.490234 22 L 13.509766 22 C 13.988766 22 14.398234 21.661406 14.490234 21.191406 L 14.824219 19.476562 C 15.644978 19.166199 16.403896 18.732596 17.070312 18.185547 L 18.714844 18.751953 C 19.167844 18.907953 19.66625 18.721641 19.90625 18.306641 L 21.414062 15.691406 C 21.653063 15.276406 21.566078 14.7515 21.205078 14.4375 L 19.886719 13.291016 C 19.955282 12.869663 20 12.440716 20 12 C 20 11.559284 19.955282 11.130337 19.886719 10.708984 L 21.205078 9.5625 C 21.566078 9.2485 21.653063 8.7216406 21.414062 8.3066406 L 19.90625 5.6933594 C 19.66725 5.2783594 19.167844 5.0910937 18.714844 5.2460938 L 17.070312 5.8125 C 16.404116 5.2657937 15.644607 4.8336609 14.824219 4.5234375 L 14.490234 2.8085938 C 14.398234 2.3385937 13.988766 2 13.509766 2 L 10.490234 2 z M 12 8 C 14.209 8 16 9.791 16 12 C 16 14.209 14.209 16 12 16 C 9.791 16 8 14.209 8 12 C 8 9.791 9.791 8 12 8 z"></path>
             </svg>
-            <button className="w-8" onClick={() => onSearch(value)}>
+            <button className="w-8">
               <svg
                 fill="#FFFFFF"
                 version="1.1"
@@ -259,6 +296,7 @@ function App() {
               </svg>
             </button>
           </div>
+          {/* Pop Out Settings */}
           <div
             className={`fixed right-0 top-0 h-full w-full transform bg-[#303030] p-4 text-white transition-transform duration-700 ${
               showSettings ? "translate-x-0" : "translate-x-full"
@@ -267,14 +305,18 @@ function App() {
             <button onClick={() => setShowSettings(false)}>Close</button>
             <div class="mb-4 flex items-center">
               <input
-                id="default-checkbox"
+                id="transition-checkbox"
                 type="checkbox"
                 value=""
                 class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
                 onChange={(e) => onTranslationChange(e)}
+                checked={
+                  currentSurah.length > 0 &&
+                  Object.values(showTranslation).every((val) => val === true)
+                }
               />
               <label
-                for="default-checkbox"
+                htmlFor="translation-checkbox"
                 class="text-white0 ms-2 text-sm font-medium"
               >
                 Translation
@@ -282,13 +324,18 @@ function App() {
             </div>
             <div class="mb-4 flex items-center">
               <input
-                id="default-checkbox"
+                id="note-checkbox"
                 type="checkbox"
                 value=""
                 class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                onChange={(e) => onNoteChange(e)}
+                checked={
+                  currentSurah.length > 0 &&
+                  Object.values(showNote).every((val) => val === true)
+                }
               />
               <label
-                for="default-checkbox"
+                htmlFor="note-checkbox"
                 class="text-white0 ms-2 text-sm font-medium"
               >
                 Notes
@@ -353,7 +400,7 @@ function App() {
                     </div>
                   )}
                   {/* Note Div */}
-                  {!showNote[ayah.id] ? (
+                  {showNote[ayah.id] ? (
                     <div className="flex h-1/5 flex-col gap-4">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
