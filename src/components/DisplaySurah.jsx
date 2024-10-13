45497d77437fd1c507cc49d404c3e2bf80673c8d
mhuss03 <134786667+mhuss03@users.noreply.github.com>
2024-10-13 23:54:43 +0100

    style changes for desktop

--- src/components/DisplaySurah.jsx
+++ src/components/DisplaySurah.jsx
@@ -14,7 +14,7 @@ export default function DisplaySurah({
     <>
       <div className="flex h-auto min-h-screen w-full items-center justify-center bg-greybg py-6 text-greybg">
         {currentSurah ? (
-          <div className="flex w-4/5 flex-col items-center gap-4">
+          <div className="flex w-4/5 flex-col items-center gap-4 md:w-3/5">
             {/* Creates Card For Each Ayah  */}
             {currentSurah.map((ayah) => (
               <div
@@ -22,19 +22,19 @@ export default function DisplaySurah({
                 className="flex w-full flex-col gap-4 rounded-xl bg-primary px-4 py-6 text-greybg"
               >
                 {/* Ayah Div */}
-                <div className="ml-[5%] leading-[3.2rem] md:text-xl">
+                <div className="ml-[5%] text-center leading-[3.2rem] md:text-xl md:leading-[4rem] lg:text-3xl lg:leading-[4.5rem]">
                   {ayah["Ayah Text"]}
                 </div>
                 {/* Translation Div */}
                 {showTranslation[ayah.id] ? (
                   <div
                     onClick={() => toggleVisibility(ayah.id, "translation")}
-                    className="flex flex-col"
+                    className="flex items-center"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="w-[8%] md:w-[4%]"
+                      viewBox="12 0 24 24"
+                      className="w-10 md:w-12 lg:w-14"
                     >
                       <path
                         d="M15 18 l6 -6 l-6 -6"
@@ -43,7 +43,7 @@ export default function DisplaySurah({
                         strokeWidth="2"
                       />
                     </svg>
-                    <div className="pl-6 md:text-2xl">
+                    <div className="md:text-2xl">
                       {ayah["Saheeh International Translation"]}
                     </div>
                   </div>
@@ -51,8 +51,8 @@ export default function DisplaySurah({
                   <div onClick={() => toggleVisibility(ayah.id, "translation")}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="w-[8%] rotate-90 md:w-[4%]"
+                      viewBox="0 -4 24 24"
+                      className="w-10 rotate-90 md:w-12 lg:w-14"
                     >
                       <path
                         d="M15 18 l6 -6 l-6 -6"
@@ -66,31 +66,33 @@ export default function DisplaySurah({
                 {/* Note Div */}
                 {showNote[ayah.id] ? (
                   <div className="flex h-auto flex-col items-center gap-2">
-                    <div>
+                    <div className="w-full">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         x="0px"
                         y="0px"
                         viewBox="0 0 24 24"
                         fill="#eef2f5"
-                        className="w-[10%] md:w-[12%]"
+                        className="w-8 md:w-10 lg:w-12"
                         onClick={() => toggleVisibility(ayah.id, "note")}
                       >
                         <path d="M 6 2 C 4.9057453 2 4 2.9057453 4 4 L 4 20 C 4 21.094255 4.9057453 22 6 22 L 18 22 C 19.094255 22 20 21.094255 20 20 L 20 8 L 14 2 L 6 2 z M 6 4 L 13 4 L 13 9 L 18 9 L 18 20 L 6 20 L 6 4 z M 8 12 L 8 14 L 16 14 L 16 12 L 8 12 z M 8 16 L 8 18 L 16 18 L 16 16 L 8 16 z"></path>
                       </svg>
                     </div>
                     {/* Saved Note Div */}
-                    <div className="flex w-full flex-col justify-around gap-2 rounded-md px-2 py-4">
+                    <div className="flex w-full flex-wrap gap-2 rounded-md px-2 py-4">
                       {savedNote[ayah.id]?.length > 0 &&
                         savedNote[ayah.id].map((note, index) => (
                           <div
-                            className="flex flex-col gap-2 rounded-md bg-greybg px-4 py-2 text-primary"
+                            className="flex w-full flex-col justify-between gap-2 rounded-md bg-greybg px-4 py-2 text-primary md:w-1/4"
                             key={`${ayah.id}-${index}`}
                           >
-                            <h3 className="text-2xl font-semibold">
-                              {note.title}
-                            </h3>
-                            <p>{note.description}</p>
+                            <div>
+                              <h3 className="text-2xl font-semibold">
+                                {note.title}
+                              </h3>
+                              <p>{note.description}</p>
+                            </div>
                             <div className="flex justify-center gap-4">
                               <button
                                 className="rounded-md bg-secondary px-2 py-1 text-greybg"
@@ -110,7 +112,7 @@ export default function DisplaySurah({
                           </div>
                         ))}
                     </div>
-                    <div className="flex w-4/5 flex-col justify-center gap-2">
+                    <div className="flex w-4/5 flex-col justify-center gap-2 md:w-1/4">
                       <button
                         className="rounded-md bg-secondary px-2 py-1 text-greybg"
                         onClick={() => handleAddNote(ayah.id, false)}
@@ -127,7 +129,7 @@ export default function DisplaySurah({
                       y="0px"
                       viewBox="0 0 24 24"
                       fill="#4fa6af"
-                      className="w-[10%] md:w-[4%]"
+                      className="w-8 md:w-10 lg:w-12"
                       onClick={() => toggleVisibility(ayah.id, "note")}
                     >
                       <path d="M 6 2 C 4.9057453 2 4 2.9057453 4 4 L 4 20 C 4 21.094255 4.9057453 22 6 22 L 18 22 C 19.094255 22 20 21.094255 20 20 L 20 8 L 14 2 L 6 2 z M 6 4 L 13 4 L 13 9 L 18 9 L 18 20 L 6 20 L 6 4 z M 8 12 L 8 14 L 16 14 L 16 12 L 8 12 z M 8 16 L 8 18 L 16 18 L 16 16 L 8 16 z"></path>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,7 +24,7 @@ export default function Modal({
   return ReactDom.createPortal(
     <>
       <div className="fixed right-0 top-0 z-[1000] flex h-full w-full items-center justify-center backdrop-blur-sm">
-        <div className="h-3/5 w-3/4 rounded-md border-2 border-accent bg-greybg px-6 py-4">
+        <div className="h-3/5 w-3/4 rounded-md border-2 border-accent bg-greybg px-6 py-4 md:w-3/5 lg:h-3/5 lg:w-2/5 xl:h-1/2">
           <div className="flex justify-between">
             <h1 className="text-xl font-semibold text-accent">
               Add a new Note
@@ -34,8 +34,8 @@ export default function Modal({
             </button>
           </div>
           <hr />
-          <div className="mt-2 flex flex-col justify-around gap-4">
-            <div className="flex flex-col">
+          <div className="mt-2 flex h-4/5 flex-col items-center justify-around">
+            <div className="flex w-full flex-col md:w-4/5">
               <label htmlFor="title" className="text-lg">
                 Title
               </label>
@@ -43,21 +43,21 @@ export default function Modal({
                 type="text"
                 name="title"
                 id="title-input"
-                className="rounded-lg border-2 border-accent pl-2"
+                className="rounded-lg border-2 border-accent py-1 pl-2 md:py-2"
                 onChange={(e) => onNoteChange(e, "title")}
               />
             </div>
-            <div className="flex flex-col text-lg">
+            <div className="flex w-full flex-col text-lg md:w-4/5">
               <label htmlFor="description">Description</label>
               <textarea
                 name="description"
                 id=""
-                className="h-40 rounded-lg border-2 border-accent pl-2"
+                className="h-40 rounded-lg border-2 border-accent py-1 pl-2 md:py-2"
                 onChange={(e) => onNoteChange(e, "description")}
               ></textarea>
             </div>
             <button
-              className="rounded-md bg-primary py-2 text-greybg"
+              className="w-3/5 rounded-md bg-primary py-2 text-greybg md:w-1/2"
               onClick={onSave}
             >
               Add Note
